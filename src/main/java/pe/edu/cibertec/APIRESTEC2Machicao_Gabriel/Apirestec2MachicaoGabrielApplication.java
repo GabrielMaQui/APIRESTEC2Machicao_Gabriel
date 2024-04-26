6d18bc68d7673396492b74dab9c12f427f621a15
Gabriel <gmachicaoq@unsa.edu.pe>
2024-04-26 15:16:12 -0500

    Iniciando el proyecto ApiRest

---
+++ src/main/java/pe/edu/cibertec/APIRESTEC2Machicao_Gabriel/Apirestec2MachicaoGabrielApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.APIRESTEC2Machicao_Gabriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apirestec2MachicaoGabrielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apirestec2MachicaoGabrielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/APIRESTEC2Machicao_Gabriel/Apirestec2MachicaoGabrielApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.APIRESTEC2Machicao_Gabriel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apirestec2MachicaoGabrielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
