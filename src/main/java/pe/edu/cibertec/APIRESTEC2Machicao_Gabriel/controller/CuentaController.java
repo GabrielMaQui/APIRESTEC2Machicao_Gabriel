82c2d93dc51017e105c10334c7f0ab6f9715a2a5
Gabriel <gmachicaoq@unsa.edu.pe>
2024-04-26 16:16:23 -0500

    Implementacion del controller de cuenta, con el mismo método de listar

--- src/main/java/pe/edu/cibertec/APIRESTEC2Machicao_Gabriel/controller/CuentaController.java
+++ src/main/java/pe/edu/cibertec/APIRESTEC2Machicao_Gabriel/controller/CuentaController.java
@@ -1,27 +1,28 @@
 package pe.edu.cibertec.APIRESTEC2Machicao_Gabriel.controller;
 
+
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pe.edu.cibertec.APIRESTEC2Machicao_Gabriel.model.bd.Persona;
-import pe.edu.cibertec.APIRESTEC2Machicao_Gabriel.service.IPersonaService;
+import pe.edu.cibertec.APIRESTEC2Machicao_Gabriel.model.bd.Cuenta;
+import pe.edu.cibertec.APIRESTEC2Machicao_Gabriel.service.ICuentaService;
 
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping(path = "api/v1/persona")
-public class PersonaController {
-    private IPersonaService personaService;
+@RequestMapping(path = "api/v1/cuenta")
+public class CuentaController {
+    private ICuentaService cuentaService;
 
     @GetMapping("")
-    public ResponseEntity<List<Persona>> listarPersona() {
+
+    public ResponseEntity<List<Cuenta>> listarCuentas() {
         return new ResponseEntity<>(
-                personaService.listarPersona(),
+                cuentaService.listarCuenta(),
                 HttpStatus.OK
         );
     }
